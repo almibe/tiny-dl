8c5e7c7de816fcbc646fbbea067c063b21ad195c
Alex Michael Berry <alexmiberry@gmail.com>
2024-09-11 17:59:11 -0400

    adding Equalivant

--- tiny-dl.spec.ts
+++ tiny-dl.spec.ts
@@ -1,13 +1,51 @@
 import { expect, test } from 'vitest'
-import { infer, Statements, SubConcept } from './tiny-dl.ts'
+import { infer, Statements, Subsumption, IsA, Equalivant } from './tiny-dl.ts'
 import { List, Set } from 'immutable'
 
 test('basic infer', () => {
-    const aBox: Statements = Set([List(["betty", ":", "Cat"])])
-    const tBox: Statements = Set([List(["Cat", SubConcept, "Animal"])])
+    const aBox: Statements = Set([List(["betty", IsA, "Cat"])])
+    const tBox: Statements = Set([List(["Cat", Subsumption, "Animal"])])
 
     expect(infer(tBox, aBox)).toEqual(Set([
-        List(["betty", ":", "Cat"]),
-        List(["betty", ":", "Animal"])
+        List(["betty", IsA, "Cat"]),
+        List(["betty", IsA, "Animal"])
+    ]))
+})
+
+test('extented infer', () => {
+    const aBox: Statements = Set([List(["betty", IsA, "Cat"])])
+    const tBox: Statements = Set([
+        List(["Cat", Subsumption, "Vetebrate"]),
+        List(["Vetebrate", Subsumption, "Animal"]),
+    ])
+
+    expect(infer(tBox, aBox)).toEqual(Set([
+        List(["betty", IsA, "Cat"]),
+        List(["betty", IsA, "Animal"]),
+        List(["betty", IsA, "Vetebrate"])
+    ]))
+})
+
+test('equivalency', () => {
+    const aBox: Statements = Set([List(["betty", IsA, "HouseCat"])])
+    const tBox: Statements = Set([
+        List(["HouseCat", Equalivant, "DomesticCat"]),
+    ])
+
+    expect(infer(tBox, aBox)).toEqual(Set([
+        List(["betty", IsA, "HouseCat"]),
+        List(["betty", IsA, "DomesticCat"]),
+    ]))
+})
+
+test('equivalency flipped', () => {
+    const aBox: Statements = Set([List(["betty", IsA, "HouseCat"])])
+    const tBox: Statements = Set([
+        List(["DomesticCat", Equalivant, "HouseCat"]),
+    ])
+
+    expect(infer(tBox, aBox)).toEqual(Set([
+        List(["betty", IsA, "HouseCat"]),
+        List(["betty", IsA, "DomesticCat"]),
     ]))
 })
--- tiny-dl.ts
+++ tiny-dl.ts
@@ -1,26 +1,43 @@
 import { List, Set } from "immutable";
-import { aB } from "vitest/dist/chunks/reporters.C_zwCd4j.js";
 
 export type Symbol = string
 export type Statement = List<Symbol>
 export type Statements = Set<Statement>
 
-export const SubConcept = "⊑"
+export const Equalivant = "≡"
+export const Subsumption = "⊑"
+export const IsA = ":"
 
 export function infer(tBox: Statements, aBox: Statements): Statements {
     let results: Statements = Set()
-    tBox.forEach((rule) => {
-        if (rule.get(1) == SubConcept) {
-            const subConcept = rule.get(0)
-            const concept = rule.get(2) as string
-            aBox.forEach(statement => {
-                if (statement.get(1) == ":" && statement.get(2) == subConcept) {
-                    results = results.add(List([statement.get(0) as string, ":", concept]))
-                }
-            })
-        } else {
-            throw "Unknown Rule! " + rule.get(1)
-        }
-    })
-    return results.union(aBox)
+    let lastResults: undefined | Statements = undefined
+    let workingABox = aBox
+    while (results != lastResults) {
+        tBox.forEach((rule) => {
+            if (rule.get(1) == Subsumption) {
+                const subConcept = rule.get(0)
+                const concept = rule.get(2) as string
+                workingABox.forEach(statement => {
+                    if (statement.get(1) == IsA && statement.get(2) == subConcept) {
+                        results = results.add(List([statement.get(0) as string, IsA, concept]))
+                    }
+                })
+            } else if (rule.get(1) == Equalivant) {
+                const left = rule.get(0) as string
+                const right = rule.get(2) as string
+                workingABox.forEach(statement => {
+                    if (statement.get(1) == IsA && statement.get(2) == left) {
+                        results = results.add(List([statement.get(0) as string, IsA, right]))
+                    } else if (statement.get(1) == IsA && statement.get(2) == right) {
+                        results = results.add(List([statement.get(0) as string, IsA, left]))
+                    }
+                })
+            } else {
+                throw "Unknown Rule! " + rule.get(1)
+            }   
+            lastResults = results;
+            workingABox = results.union(workingABox);
+        })
+    }
+    return results.union(workingABox)
 }
